c787b2c1c2573b9efe7bf765a4ff1b6617ca1343
Gabriel dos Santos Sanches <gabrielssanches@gmail.com>
2024-09-23 11:40:59 +0200

    docker: adds wasm build

--- docker/make.sh
+++ docker/make.sh
@@ -5,3 +5,9 @@ docker run \
     -it \
     gabrielssanches/raylib-linux_x64:5.0 \
     /bin/bash -c "source /usr/local/bin/entrypoint.sh && make all"
+
+docker run \
+    -v `pwd`:/game \
+    -it \
+    gabrielssanches/raylib-wasm:5.0 \
+    /bin/bash -c "source /usr/local/bin/entrypoint.sh && make all"
